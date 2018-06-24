ea802d70d782e9ef902cc45ee4fc7cc83c903062
A Muttaki Khan <36711043+muttaki1251998@users.noreply.github.com>
2018-06-24 06:55:29 +0600

    admin page post request and error done

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const flash = require('connect-flash');
 const config = require('./config/database');
 
 //get route paths
@@ -27,6 +32,48 @@ const port = process.env.PORT || 4000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+// session mw
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// passport mw
+app.use(passport.initialize());
+app.use(passport.session());
+
+// express validator mw
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+            , root    = namespace.shift()
+            , formParam = root;
+
+        while(namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param : formParam,
+            msg   : msg,
+            value : value
+        };
+    }
+}));
+
+// connect-flash
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+//express-messages middleware
+app.use(function(req, res, next){
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+});
+
 // set view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -37,6 +84,13 @@ app.use('/admin', adminRouter);
 
 // static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// set errors variable
+ app.locals.errors = null;
+ 
+app.use(function(err, req, res, next){
+    res.locals.message = err.message;
+});
 
 app.listen(port, () => {
 	console.log("Server started at port 4000");
--- routes/admin_page.js
+++ routes/admin_page.js
@@ -1,8 +1,48 @@
 const express = require('express');
+const expressValidator = require('express-validator');
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.send('Admin area');
+});
+
+// add pages
+router.get('/add-page', (req, res) => {
+   var title = '';
+   var content = '';
+//    var slug = '';
+   
+   res.render('admin/add-page', {
+       title: title,
+    //    slug: slug,
+       content: content
+   });
+});
+
+router.post('/add-page', (req, res) => {
+
+    var title = req.body.title;
+    // var slug = req.body.slug.replace(/\s+/g, '-').toLowecase();
+    // if (slug == "") slug = title.replace(/\s+/g, '-').toLowecase();       
+    var content = req.body.content;
+
+    req.checkBody('title', "Title cannot be empty").notEmpty();
+    req.checkBody('content', "Content must be filled").notEmpty();    
+
+    var errors = req.validationErrors();
+
+    if(errors){
+        res.render('admin/add-page', {
+            errors: errors,
+            title: title,
+            content: content,
+            // slug: slug
+        });
+        console.log(errors.msg);
+        
+    }else{
+        console.log('Success');
+    }
 });
 
 module.exports = router;
